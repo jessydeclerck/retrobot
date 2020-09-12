cf3a6c461336cc2d97f8e905417b783d5c87e33f
Not Ultiko <ljclaeyssen@gmail.com>
2020-09-12 19:30:55 +0200

    resize

--- src/main/java/automation/NativeWindowsEvents.java
+++ src/main/java/automation/NativeWindowsEvents.java
@@ -11,18 +11,31 @@ public class NativeWindowsEvents {
     public static final int WM_LBUTTONUP = 514;
     public static final int WM_LBUTTONDOWN = 513;
     final static String winTitle = "Dopeultiko-[HOG] - Dofus Retro v1.32.1";
+    final static User32 user32 = Native.load("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
+    public static WinDef.HWND hwnd;
 
     public static void clic(int x, int y) {
-        final User32 user32 = Native.load("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
-
-        WinDef.HWND hwnd = user32.FindWindow(null, winTitle);
 
         long param = x + (y << 16);
         WinDef.LPARAM lparam = new WinDef.LPARAM(param);
         WinDef.WPARAM wparam = new WinDef.WPARAM(0);
 
+
+
         user32.SendMessage(hwnd, WM_LBUTTONDOWN, wparam, lparam);
         user32.SendMessage(hwnd, WM_LBUTTONUP, wparam, lparam);
+    }
+
+    public static void prepareForAutomation(){
+        hwnd = user32.FindWindow(null, winTitle);
+        /**
+         * i = x
+         * i1 = y
+         * i2 = largeur
+         * i3 = hauteur
+         * i4 = yolo
+         */
+        user32.SetWindowPos(hwnd, hwnd, 300, 300, 300, 300, 300);
     }
 
 }
