843fb28b7a7b4bee231861790b06a49a976f7a68
Jessy Declerck <declerck.jessy@gmail.com>
2020-09-14 19:45:53 +0200

    feat: fix priority queue

--- src/main/java/async/RetroTaskEventConsumer.java
+++ src/main/java/async/RetroTaskEventConsumer.java
@@ -3,12 +3,14 @@ package async;
 import async.event.RecolterTaskEvent;
 import lombok.extern.log4j.Log4j2;
 
-import java.util.concurrent.BlockingDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
 
 @Log4j2
 public class RetroTaskEventConsumer implements Runnable {
 
-    private final BlockingDeque<RecolterTaskEvent> taskQueue = RetroTaskQueue.getInstance().getTaskQueue();
+    private final PriorityBlockingQueue<RecolterTaskEvent> taskQueue = RetroTaskQueue.getInstance().getTaskQueue();
 
     @Override
     public void run() {
@@ -16,8 +18,12 @@ public class RetroTaskEventConsumer implements Runnable {
             try {
                 log.debug("Waiting for a task");
                 RecolterTaskEvent event = taskQueue.take();
-                log.info("Processing task {} : {}", event.getRessourceCell().id(), event);
-                event.execute();
+                log.debug("Processing task {} : {}", event.getRessourceCell().id(), event);
+                if(event.getProcessCount() > 1){
+                    log.debug("Discarding task (too many retries) {} : {}", event.getRessourceCell().id(), event);
+                } else {
+                    event.execute();
+                }
             } catch (InterruptedException e) {
                 log.error("Queue task has beeen interrupted", e);
             }
