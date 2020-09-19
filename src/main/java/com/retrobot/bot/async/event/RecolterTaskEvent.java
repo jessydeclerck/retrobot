f310c62133c8600fc981a6c8839446de440d82d6
Jessy Declerck <declerck.jessy@gmail.com>
2020-09-20 01:00:45 +0200

    fix: fix unicity of events

--- src/main/java/com/retrobot/bot/async/event/RecolterTaskEvent.java
+++ src/main/java/com/retrobot/bot/async/event/RecolterTaskEvent.java
@@ -14,17 +14,19 @@ import lombok.extern.slf4j.Slf4j;
 @ToString
 public class RecolterTaskEvent {
 
+    @EqualsAndHashCode.Exclude
     private final Class producer;
 
     private final RetroBotTask retroBotTask = RetroBotTask.RECOLTER;
 
     private final RetroRessourceCell ressourceCell;
 
+    @EqualsAndHashCode.Exclude
     private int processCount = 0; //number of time we tried to process this event
 
     public RecolterTaskEvent(RetroRessourceCell ressourceCell, Class producer) {
         this.ressourceCell = ressourceCell;
-        this.producer=producer;
+        this.producer = producer;
     }
 
     public void incrementProcessCount() {
