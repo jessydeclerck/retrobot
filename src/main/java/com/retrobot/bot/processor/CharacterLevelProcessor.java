f1860f968c784536389aae28b942fac457000e58
Jessy Declerck <declerck.jessy@gmail.com>
2020-09-20 16:24:26 +0200

    feat: close character level up popup

---
+++ src/main/java/com/retrobot/bot/processor/CharacterLevelProcessor.java
@@ -0,0 +1,22 @@
+package com.retrobot.bot.processor;
+
+import com.retrobot.utils.TimeUtils;
+import com.retrobot.utils.automation.NativeWindowsEvents;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CharacterLevelProcessor extends PacketProcessor {
+    @Override
+    public void processPacket(String dofusPacket) {
+        log.info("Fermeture notification niveau personnage");
+        TimeUtils.sleep(1000);
+        NativeWindowsEvents.clic(481, 387);
+    }
+
+    @Override
+    public String getPacketId() {
+        return "AN";
+    }
+}
